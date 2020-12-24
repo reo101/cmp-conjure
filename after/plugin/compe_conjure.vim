a441174cf1e0203254a23c59995c3789e08058c7
Tami <tamibam@yahoo.com>
2020-12-24 21:24:41 +0300

    Add documentation preview

--- after/plugin/compe_conjure.vim
+++ after/plugin/compe_conjure.vim
@@ -1,3 +1,4 @@
 if exists('g:loaded_compe')
+  packadd conjure
   lua require'compe'.register_source('conjure', require'compe_conjure')
 endif
--- lua/compe_conjure/init.lua
+++ lua/compe_conjure/init.lua
@@ -1,6 +1,7 @@
 local Source = {}
-local ConjureEval = require'conjure.eval'
-local ConjurePromise = require'conjure.promise'
+local conjure_eval = require'conjure.eval'
+local conjure_promise = require'conjure.promise'
+local util = require'vim.lsp.util'
 
 function Source.new()
   local self = setmetatable({}, { __index = Source })
@@ -12,7 +13,7 @@ end
 function Source.get_metadata(self)
   return {
     priority = 1000;
-    filetypes = vim.g.compe_conjure_fts or {"fennel", "janet"};
+    filetypes = vim.g.compe_conjure_fts or {"fennel", "janet", "racket", "clojure"};
     dub = 0;
     menu = '[conjure]';
   }
@@ -24,34 +25,52 @@ function Source.datermine(self, context)
     return {}
   end
   local trigger
-  if vim.fn.index({'.'}, context.before_char) >= 0 then
+  if vim.fn.index({'.'}, context.before_char) >= 0 or vim.fn.index({'/'}, context.before_char) >= 0 then
     trigger = context.col
   else
     trigger = -1
   end
-  if vim.bo.filetype == 'fennel' then
+  -- if vim.bo.filetype == 'fennel' then
     return {
       keyword_pattern_offset = offset + 1,
       trigger_character_offset =  trigger
     }
-  else
-    return { keyword_pattern_offset = offset + 1, }
-  end
+  -- else
+    -- return { keyword_pattern_offset = offset + 1, }
+  -- end
 end
 
 function Source.complete(self, args)
   self:abort()
   local input = args.context:get_input(args.keyword_pattern_offset)
-  self.promise_id = ConjureEval['completions-promise'](input)
+  self.promise_id = conjure_eval['completions-promise'](input)
   self.timer = vim.loop.new_timer()
   self.timer:start(100, 100, vim.schedule_wrap(function()
-    if ConjurePromise['done?'](self.promise_id) then
+    if conjure_promise['done?'](self.promise_id) then
       args.callback({
-        items = ConjurePromise.close(self.promise_id)
+        items = conjure_promise.close(self.promise_id)
       })
       self:abort()
     end
   end))
+end
+
+function Source.documentation(self, args)
+  local document = {}
+  local content = args.completed_item.info
+  -- if content ~= '' then
+  --   table.insert(document, '```' .. vim.bo.filetype .. '\n')
+  --   for _, v in ipairs(vim.split(content, "\n")) do
+  --     table.insert(document, v)
+  --   end
+  --   table.insert(document, '```')
+  --   -- print(vim.inspect(document))
+  -- end
+  if content ~= '' then
+    args.callback(content)
+  else
+    args.abort()
+  end
 end
 
 function Source.abort(self)
@@ -61,7 +80,7 @@ function Source.abort(self)
     self.timer = nil
   end
   if self.promise_id then
-    ConjurePromise.close(self.promise_id)
+    conjure_promise.close(self.promise_id)
     self.promise_id = nil
   end
 end
